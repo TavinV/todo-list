1541c851f06cceefab8d0cb865bcd4a7890fb59a
TavinV <otavioviniciusads@gmail.com>
2024-07-10 00:35:04 -0300

    adicionei funcionalidade do arquivo json

--- main.js
+++ main.js
@@ -10,3 +10,28 @@ toggle_darkmode.addEventListener('click', () => {
 open_github.addEventListener('click', () => {
     window.open("https://github.com/TavinV")
 })
+
+// Adicionar tarefas do json
+
+const tarefas = document.querySelector('ul')
+
+fetch('./tarefas.json')
+    .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            data.forEach(tarefa => {
+                tarefas.innerHTML += `<li>
+                    <div id="${tarefa.id}" class="tarefa">
+                        <span class="nome-da-tarefa">
+                            <button class="tarefa-feita"><i id="check" class="fa-solid fa-check"></i></button>
+                            <h2>${tarefa.titulo}</h2>
+                        </span>
+                        
+                        <span class="controlar-tarefa">
+                            <button class="editar-tarefa"> <i id="pen-icon" class="fa-solid fa-pen-to-square"> </i> </button>
+                            <button clas="apagar-tarefa"><i id="trash-icon" class="fa-solid fa-trash"></i></button>
+                        </span>
+                    </div>
+                </li>`
+            });
+        })
